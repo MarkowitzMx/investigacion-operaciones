d9cda4f38cdd8818a337ed6aad7a1fb62151667f
MarkowitzMx <139413265+MarkowitzMx@users.noreply.github.com>
2026-03-16 04:29:05 +0000

    fix parenthesis

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ import os
 
 # -------------------------------------------------
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(BASE_DIR)
 
 # -------------------------------------------------
